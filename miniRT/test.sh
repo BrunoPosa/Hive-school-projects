67921e06bb068bc0a54557ebfe16631c59378893
Jack Waddington <jack@waddington.me>
2024-11-07 15:13:05 +0200

    added miniRT directory

---
+++ miniRT/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clear the terminal screen
+clear
+
+# Recompile the raytracer
+make re
+make clean
+
+# Run the raytracer
+# ./file_to_list test_short.rt
+./miniRT scenes/test_1_sphere.rt > log.txt && vim log.txt
+
+
+# Display the image
+#cat output.ppm
