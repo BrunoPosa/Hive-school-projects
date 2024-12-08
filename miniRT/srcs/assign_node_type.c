66f1a2a7f8a4113296c1ae98de2a3f2dc80a3955
Jack Waddington <jack@waddington.me>
2024-12-08 19:23:12 +0200

    norm

--- miniRT/srcs/assign_node_type.c
+++ miniRT/srcs/assign_node_type.c
@@ -1,78 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   assign_node_type.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwadding <jwadding@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 19:18:55 by jwadding          #+#    #+#             */
+/*   Updated: 2024/12/08 19:22:44 by jwadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/file_to_list.h"
 
-int move_past_type(t_list **l)
+int	move_past_type(t_list **l)
 {
-    t_list *current;
+	t_list	*current;
 
-    current = *l;
-
-    while (current)
-    {
-    if(current->type == ambiant)
-        current->p = current->p + 1;
-    else if(current->type == camera)
-        current->p = current->p + 1;
-    else if(current->type == light)
-        current->p = current->p + 1;
-    else if(current->type == plane)
-        current->p = current->p + 2;
-    else if(current->type == sphere)
-        current->p = current->p + 2;
-    else if(current->type == cylinder)
-        current->p = current->p + 2;
-    current->p = skip_space(current->p);
-    current = current->next;
-    }
-    return (SUCCESS);
+	current = *l;
+	while (current)
+	{
+		if (current->type == ambiant)
+			current->p = current->p + 1;
+		else if (current->type == camera)
+			current->p = current->p + 1;
+		else if (current->type == light)
+			current->p = current->p + 1;
+		else if (current->type == plane)
+			current->p = current->p + 2;
+		else if (current->type == sphere)
+			current->p = current->p + 2;
+		else if (current->type == cylinder)
+			current->p = current->p + 2;
+		current->p = skip_space(current->p);
+		current = current->next;
+	}
+	return (SUCCESS);
 }
 
 int	assign_node_type(t_list **l)
 {
-    t_list *current;
+	t_list	*current;
 
-    current = *l;
-
-    while (current)
-    {
-        if (!ft_strncmp(current->p, "A", 1))
-            current->type = ambiant;
-        else if (!ft_strncmp(current->p, "C", 1))
-            current->type = camera;
-        else if (!ft_strncmp(current->p, "L", 1))
-            current->type = light;
-        else if (!ft_strncmp(current->p, "pl", 2))
-            current->type = plane;
-        else if (!ft_strncmp(current->p, "sp", 2))
-            current->type = sphere;
-        else if (!ft_strncmp(current->p, "cy", 2))
-            current->type = cylinder;
-        else
-            return (ret_error(E_WRONG_TYPE, *l));
-        current = current->next;
-    }
-    move_past_type(l);
-    return (SUCCESS);
+	current = *l;
+	while (current)
+	{
+		if (!ft_strncmp(current->p, "A", 1))
+			current->type = ambiant;
+		else if (!ft_strncmp(current->p, "C", 1))
+			current->type = camera;
+		else if (!ft_strncmp(current->p, "L", 1))
+			current->type = light;
+		else if (!ft_strncmp(current->p, "pl", 2))
+			current->type = plane;
+		else if (!ft_strncmp(current->p, "sp", 2))
+			current->type = sphere;
+		else if (!ft_strncmp(current->p, "cy", 2))
+			current->type = cylinder;
+		else
+			return (ret_error(E_WRONG_TYPE, *l));
+		current = current->next;
+	}
+	move_past_type(l);
+	return (SUCCESS);
 }
-
 
 // if current->p[0] == num, ',' 
 //     return error
-
-
 // if (!strcmp(current->p[0] && !strcmp(s[1] == ' '))
-
-
-
 // A
 // C
 // L
 // pl
 // sp
 // cy
-
 // check the first letters of current->p are etc.
-
 // update the pointer, of the s.
-
 // update the type enum.
-
--- miniRT/srcs/main.c
+++ miniRT/srcs/main.c
@@ -1,5 +1,14 @@
-#include "../inc/file_to_list.h"
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwadding <jwadding@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 19:04:23 by jwadding          #+#    #+#             */
+/*   Updated: 2024/12/08 19:05:50 by jwadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 int	main(int argc, char **argv)
 {
@@ -12,22 +21,18 @@ int	main(int argc, char **argv)
 	scene = ft_calloc(1, sizeof(t_scene));
 	if (!scene)
 		return (ret_error(E_MALLOC, NULL));
-
 	if (parse(argc, argv, scene) != SUCCESS)
 		return (free(scene), ERROR);
-
 	mlx = mlx_init(WINSIZE, WINSIZE, "minirt", false);
 	if (!(mlx))
 		return (free(scene), ERROR);
 	img = mlx_new_image(mlx, WINSIZE, WINSIZE);
 	if (!img || mlx_image_to_window(mlx, img, 0, 0) < 0)
 		return (free(scene), mlx_terminate(mlx), ERROR);
-
 	if (render_pixels(scene, img) != SUCCESS)
 		return (mlx_terminate(mlx), ERROR);
-
 	mlx_key_hook(mlx, &esc_keyhook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
-	return(free(scene), SUCCESS);
+	return (free(scene), SUCCESS);
 }
