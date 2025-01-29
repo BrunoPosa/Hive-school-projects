d20365472acc61bca530620b69e468d97478c6da
Bruno Posa <bposa@student.hive.fi>
2025-01-30 00:55:07 +0200

    cpp--01 - 05 tests

--- cpp/cpp01/ex05/main.cpp
+++ cpp/cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 23:00:26 by bposa             #+#    #+#             */
-/*   Updated: 2025/01/30 00:39:58 by bposa            ###   ########.fr       */
+/*   Updated: 2025/01/30 00:54:20 by bposa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 int	main(void)
 {
-	Harl	Harly;
+	try
+	{
+		Harl	Harly;
 
-	Harly.complain("HELLO");
-	Harly.complain("INFO");
-	Harly.complain("DEBUG ");
+		Harly.complain("HELLO");
+		Harly.complain("INFO");
+		Harly.complain("DEBUG ");
+		Harly.complain("DEBUG");
+		Harly.complain(nullptr);
+
+	}
+	catch(const std::exception& e)
+	{
+		cout << e.what() << endl;
+		return 1;
+	}
 	return 0;
 }
