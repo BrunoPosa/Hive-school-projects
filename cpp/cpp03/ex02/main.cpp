8812ce15d691692da3e11e0094c3b3310545c437
bposa <bruno.posa@gmail.com>
2025-02-17 15:41:13 +0200

    cpp--03 -02 done

--- cpp/cpp03/ex02/main.cpp
+++ cpp/cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 21:10:26 by bposa             #+#    #+#             */
-/*   Updated: 2025/02/17 14:31:14 by bposa            ###   ########.fr       */
+/*   Updated: 2025/02/17 15:40:47 by bposa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,33 @@
 int main()
 {
 	FragTrap	frog("Froggie");
+	ClapTrap	clog("Cloggie");
+
+	cout << endl;
+	frog.print();
+	clog.print();
+	cout << endl;
 
 	frog.attack("Mom");
-	frog.print();
+	clog.takeDamage(30);
 	frog.highFivesGuys();
-	frog.beRepaired(1);
+	frog.beRepaired(42);
+	clog.beRepaired(42);
+	clog.attack("Space, shaking fist in air angrily");
+	cout << endl;
+
+	ClapTrap	avenger("Starkie Tony");
+	avenger.beRepaired(10000);
+	FragTrap	thanos("Tiki Tiki");
+	frog = thanos;
+	avenger.attack("Frogsmith lineage");
+	frog.takeDamage(10000);
+	thanos.takeDamage(1000);
+	FragTrap(frog);
+
+	cout << endl;
 	frog.print();
+	clog.print();
+	avenger.print();
 	return 0;
 }
