c964200913d4b0ecf62cc5d7dced30d031eab13e
bposa <bruno.posa@gmail.com>
2025-02-17 15:46:12 +0200

    cpp--03 -02 fix

--- cpp/cpp03/ex00/main.cpp
+++ cpp/cpp03/ex00/main.cpp
@@ -6,14 +6,13 @@
 /*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 20:05:16 by bposa             #+#    #+#             */
-/*   Updated: 2025/02/10 20:49:36 by bposa            ###   ########.fr       */
+/*   Updated: 2025/02/17 15:43:55 by bposa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int	main (void)
-{
 {
 	string name = "Doozy";
 	ClapTrap a(name);
@@ -25,34 +24,6 @@ int	main (void)
 	a.takeDamage(2);
 	a.print();
 	cout << endl << endl;
-}
-{
-	ClapTrap	a("CT A");
-	ClapTrap	b("CT B");
-
-	a.print();
-	b.print();
-	b.takeDamage(3);
-	a.takeDamage(6);
-	a.attack("CT B");
-	b.attack("noob1");
-	a.print();
-	b.print();
-	b.beRepaired(4);
-	b.attack("CT B");
-	b.attack("CT B");
-	a.beRepaired(1);
-	a.print();
-	b.print();
-	b.takeDamage(12);
-	a.takeDamage(9);
-	a.beRepaired(5);
-	b.attack("CT3");
-	a.print();
-	b.print();
-	b.takeDamage(12);
-	b.print();
-}
 	return 0;
 
 }
