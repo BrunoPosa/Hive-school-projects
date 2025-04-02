213e32fb9286d01d393b8e195de013dd44190713
Bruno Posa <bposa@student.hive.fi>
2025-04-02 17:48:36 +0300

    cpp--06 - ex00 save state

---
+++ cpp/cpp06/ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 15:23:16 by bposa             #+#    #+#             */
+/*   Updated: 2025/04/02 16:01:02 by bposa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	TODO:
+	-class ScalarConverter OCF
+	-"For each exercise, type conversion must be handled using a specific type of casting. Your choice will be reviewed during the defense."
+	-is_scalar class
+*/
+int main (void) {
+
+	return 0;
+}
