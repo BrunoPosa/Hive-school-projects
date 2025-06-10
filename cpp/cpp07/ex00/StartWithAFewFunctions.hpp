4fa001434f0b74d792e5751fc46a20c2fd0c0902
Bruno Posa <bposa@student.hive.fi>
2025-06-10 20:56:04 +0300

    cpp--07 - ex00 save state

--- cpp/cpp07/ex00/StartWithAFewFunctions.hpp
+++ cpp/cpp07/ex00/StartWithAFewFunctions.hpp
@@ -1,3 +1,21 @@
+#ifndef STARTWITHAFEWFUNCTIONS_HPP
+# define STARTWITHAFEWFUNCTIONS_HPP
+
+#include <iostream>
+#include <string>
+
 template <typename T> T min(const T& l, const T& r) {
-	return l < r	? l : r
+	return l < r	? l : r;
 }
+
+template <typename T> T max(const T& l, const T& r) {
+	return l > r	? l : r;
+}
+
+template <typename T> void swap(T& l, T& r) {
+	T temp = l;
+	l = r;
+	r = temp;
+}
+
+#endif
--- cpp/cpp07/ex00/main.cpp
+++ cpp/cpp07/ex00/main.cpp
@@ -1,6 +1,6 @@
-//given main
-#include <iostream>
+#include "StartWithAFewFunctions.hpp"
 
+//subject main
 int main( void ) {
 	int a = 2;
 	int b = 3;
