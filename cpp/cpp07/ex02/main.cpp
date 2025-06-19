02d772ae79f554529856160c8caa107e07a84590
Bruno Posa <bposa@student.hive.fi>
2025-06-20 02:53:04 +0300

    cpp--07 - ex02 save state

--- cpp/cpp07/ex02/main.cpp
+++ cpp/cpp07/ex02/main.cpp
@@ -9,45 +9,45 @@ namespace {
 
 	public:
 		TestClass() : _value(42) {
-			std::cout << "TestClass default constructor\n";
+			cout << "TestClass default constructor" << endl;
 		}
 		TestClass(int v) : _value(v) {
-			std::cout << "TestClass param constructor\n";
+			cout << "TestClass param constructor" << endl;
 		}
 		TestClass(const TestClass& other) : _value(other._value) {
-			std::cout << "TestClass copy constructor\n";
+			cout << "TestClass copy constructor" << endl;
 		}
 		TestClass& operator=(const TestClass& other) {
-			std::cout << "TestClass copy assignment\n";
+			cout << "TestClass copy assignment" << endl;
 			if (this != &other)
 				_value = other._value;
 			return *this;
 		}
 		~TestClass() {
-			std::cout << "TestClass destructor\n";
+			cout << "TestClass destructor" << endl;
 		}
 
 		int getValue() const { return _value; }
 	};
 
 	void testWithClass() {
-		std::cout << "Creating Array<TestClass> with 3 elements\n";
+		cout << "Creating Array<TestClass> with 3 elements" << endl;
 		Array<TestClass> arr(3);
 
-		std::cout << "\nAssigning new TestClass objects\n";
+		cout << "\nAssigning new TestClass objects" << endl;
 		for (unsigned int i = 0; i < arr.size(); ++i) {
 			arr[i] = TestClass(i * 100); // triggers param constructor + copy assignment
 		}
 
-		std::cout << "\nReading values back:\n";
+		cout << "\nReading values back:" << endl;
 		for (unsigned int i = 0; i < arr.size(); ++i) {
-			std::cout << "arr[" << i << "] = " << arr[i].getValue() << "\n";
+			cout << "arr[" << i << "] = " << arr[i].getValue() << endl;
 		}
 
-		std::cout << "\nTesting copy constructor\n";
+		cout << "\nTesting copy constructor" << endl;
 		Array<TestClass> copyArr(arr); // triggers deep copy
 
-		std::cout << "\nTesting copy assignment\n";
+		cout << "\nTesting copy assignment" << endl;
 		Array<TestClass> assignedArr;
 		assignedArr = arr;
 	}
@@ -64,11 +64,11 @@ int main (void) {
 
 		// Test 1: Default constructor
 		Array<int> a;
-		std::cout << "Size of default array: " << a.size() << "\n";
+		cout << "Size of default array: " << a.size() << endl;
 
 		// Test 2: Parameterized constructor
 		Array<int> b(5);
-		std::cout << "Size of parameterized array: " << b.size() << "\n";
+		cout << "Size of parameterized array: " << b.size() << endl;
 
 		// Fill array with values
 		for (unsigned int i = 0; i < b.size(); ++i) {
@@ -76,36 +76,36 @@ int main (void) {
 		}
 
 		// Read values to confirm write succeeded
-		std::cout << "Contents of array b: ";
+		cout << "Contents of array b: ";
 		for (unsigned int i = 0; i < b.size(); ++i) {
-			std::cout << b[i] << " ";
+			cout << b[i] << " ";
 		}
-		std::cout << "\n";
+		cout << endl;
 
 		// Test 3: Bounds checking
 		try {
-			std::cout << "Accessing out-of-bounds index...\n";
-			std::cout << b[10] << "\n"; // Should throw
+			cout << "Accessing out-of-bounds index..." << endl;
+			cout << b[10] << endl; // Should throw
 		} catch (const std::exception& e) {
-			std::cout << "Caught exception on out-of-bounds access\n";
+			cout << "Caught exception on out-of-bounds access" << endl;
 		}
 
 		// Test 4: Copy constructor
 		Array<int> c(b); // deep copy
-		std::cout << "Contents of copy-constructed array c: ";
+		cout << "Contents of copy-constructed array c: ";
 		for (unsigned int i = 0; i < c.size(); ++i) {
-			std::cout << c[i] << " ";
+			cout << c[i] << " ";
 		}
-		std::cout << "\n";
+		cout << endl;
 
 		// Test 5: Copy assignment
 		Array<int> d;
 		d = b;
-		std::cout << "Contents of copy-assigned array d: ";
+		cout << "Contents of copy-assigned array d: ";
 		for (unsigned int i = 0; i < d.size(); ++i) {
-			std::cout << d[i] << " ";
+			cout << d[i] << " ";
 		}
-		std::cout << "\n\n";
+		cout << endl;
 
 		testWithClass();
 	} catch (std::exception& e) {
